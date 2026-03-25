156a03641eb8f851b52328f4144e90a4b3f6e26c
Giovanna Dornelles <giovannadornelleszft@gmail.com>
2026-03-25 14:30:08 -0300

    Update executar.sh

--- executar.sh
+++ executar.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# 1. Constrói (build) a imagem usando o seu arquivo Dockerfile e dá o nome (tag) de "calc-giovanna"
+# constroi a imagem usando o e da o nome
 echo "Construindo a imagem Docker..."
 docker build -t calc-giovanna -f giovanna.Dockerfile .
 
-# 2. Executa (run) a imagem criada
-# O -d roda em segundo plano (para não travar seu terminal)
-# O -p 8080:8080 liga a porta do seu computador na porta do container
+# executa a imagem criada
 echo "Iniciando o container..."
 docker run -d -p 8080:8080 --name minha-calculadora calc-giovanna
 
